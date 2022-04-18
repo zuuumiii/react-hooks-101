24decbcc226782db705bf1cc12d2b857d05b33d4
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-18 16:24:23 +0900

    hook with useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is atemplate for React App</div>;
-};
+  const [count, setCount] = useState(0);
 
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+  const reset = () => setCount(0);
+  const double = () => setCount(count * 2);
+  const triple = () => {
+    if (count % 3 === 0) setCount(count / 3);
+  };
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={double}>double</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div>
+        <button onClick={triple}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
+  );
+};
 export default App;
