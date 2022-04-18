cfb52c8f106886fde0c6d3e9deb4269c3507938e
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-18 18:04:48 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
+  useEffect(() => {
+    console.log("useEffect");
+  });
+
+  useEffect(() => {
+    console.log("useEffect2");
+  }, []);
+
+  useEffect(() => {
+    console.log("name change");
+  }, [name]);
+
   return (
     <>
       <p>
