d737d2eed6ecb1b27418bc2d74ba2083192a0a17
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-20 11:50:03 +0900

    create operationLogs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext } from "react";
-import { DELETE_ALL_EVENTS, CREATE_EVENT } from "../actions";
+import {
+  DELETE_ALL_EVENTS,
+  CREATE_EVENT,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS,
+} from "../actions";
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils";
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -14,6 +20,11 @@ const EventForm = () => {
       title,
       body,
     });
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: "イベントを作成しました",
+      operatedAt: timeCurrentIso8601(),
+    });
     setTitle("");
     setBody("");
   };
@@ -24,6 +35,11 @@ const EventForm = () => {
     if (result) {
       dispatch({
         type: DELETE_ALL_EVENTS,
+      });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: "全てのイベントを削除しました",
+        operatedAt: timeCurrentIso8601(),
       });
     }
   };
