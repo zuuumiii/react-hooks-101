a843a20ea5e9fc0d50e0c6a76f08f21da0d08701
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-20 13:24:30 +0900

    save state with local strage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import AppContext from "../contexts/AppContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import reducer from "../reducers";
@@ -6,9 +6,18 @@ import Events from "./Events";
 import EventForm from "./EventForm";
 import OperationLogs from "./OperationLogs";
 
+const APP_KEY = "appWithRedux";
 const App = () => {
-  const initialState = { events: [], operationLogs: [] };
+  const appState = localStorage.getItem(APP_KEY);
+  const initialState = appState
+    ? JSON.parse(appState)
+    : { events: [], operationLogs: [] };
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
+
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
